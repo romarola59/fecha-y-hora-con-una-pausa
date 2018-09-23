eeb3ce831a9de1d6bb1262e92d9e4dcf94adff46
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-22 22:49:28 -0300

    Agregar archivos de proyecto.

---
+++ Fecha y hora con pausa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fecha_y_hora_con_pausa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
