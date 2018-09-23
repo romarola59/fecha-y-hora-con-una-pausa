51e56175c0ba4cfe2a6289f7f8758beb8d7efc69
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-22 23:15:11 -0300

    fechayhora3cambios

--- Fecha y hora con pausa/Program.cs
+++ Fecha y hora con pausa/Program.cs
@@ -10,11 +10,15 @@ namespace Fecha_y_hora_con_pausa
             DateTime fecha = DateTime.Now;  // inicializamos la variable fecha, con la fecha actual.
 
             Console.WriteLine("Son las {0}: {1}:{2} ",fecha.Hour,fecha.Minute,fecha.Second);
-            Console.WriteLine("Vamos a esperar 5 segundos .....");
-            Thread.Sleep(5000);  // comando para hacer la pausa en 5 segundos
+            Console.WriteLine("Vamos a esperar 10 segundos .....");
+            Thread.Sleep(10000);  // comando para hacer la pausa en 5 segundos
             fecha = DateTime.Now;  // actualizamos la fecha en Horas , minutos,segundos(en tiempo)
             Console.WriteLine("Ahora son las {0}: {1}:{2} ", fecha.Hour, fecha.Minute, fecha.Second);
-
+            Console.WriteLine("Ahora vamos a esperar medio minuto....");
+            Thread.Sleep(30000);
+            fecha = DateTime.Now;
+            Console.WriteLine("Ahora despues de medio minuto son {0}: {1}:{2}", fecha.Hour, fecha.Minute, fecha.Second);
+            Console.WriteLine("hora de dormir byeeeeeeeee.....");
             Console.ReadKey();
 
         }
